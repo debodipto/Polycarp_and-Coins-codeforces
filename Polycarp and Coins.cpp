0de1aceb5500e2bebca37e02ed3f2f114b432404
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-01-15 21:48:52 +0600

    Update Polycarp and Coins.cpp

--- Polycarp and Coins.cpp
+++ Polycarp and Coins.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() 
+{
+    int t;
+    cin >> t;
+    for (int z = 1; z <= t; z++){
+        int n;
+        cin >> n;
+        int one = n/3;
+        int two = n/3;
+
+        if(n%3 == 1){
+            one++;
+        }else if(n%3 == 2){
+            two++;
+        }
+        
+        cout << one << " " << two << endl;
+    }
+    return 0;
+}
